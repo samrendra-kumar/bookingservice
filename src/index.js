51c10c9224b814ca2f830c7c2b3fde8b27224ec6
Samrendra <samrendrakumar893@gmail.com>
2023-10-14 07:44:35 +0530

    index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express= require("express") ;
-const {PORT} = require("./config/serverConfig")
+const bodyParser = require("body-parser");
+const {PORT} = require("./config/serverConfig");
+
 require("dotenv").config() 
 
 const app = express() ;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true })); 
 app.use(express.json());
 app.listen(PORT,()=>{
     console.log(`App is running at ${PORT}`)
-})
+}) 
