49ac780946dad6bc0d27691d0df19d3b3b3b2f99
Samrendra <samrendrakumar893@gmail.com>
2023-10-16 15:08:50 +0530

    resolve update controller

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -71,7 +71,7 @@ const get = async(req,res) =>
 
 const update = async(req,res) => {
     try{
-        const response = await cityService.updateCityCity(req.params.id,req.body);
+        const response = await cityService.updateCity(req.params.id,req.body);
         return res.status(200).json({
          data:response ,
          success:true ,
