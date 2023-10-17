5d1a52dd3c5a36a22edf95758fa6a35f6682691f
Samrendra <samrendrakumar893@gmail.com>
2023-10-17 15:24:52 +0530

    seeder added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const {City} = require('./models/index')
 const {PORT} = require("./config/serverConfig");
 const ApiRoutes = require('./routes/index');
+const {Airport} = require('./models/index');
+const db=require('./models/index')
 require("dotenv").config() 
 
 const app = express() ;
@@ -12,5 +14,11 @@ app.use('/api',ApiRoutes);
 app.use(express.json());
 app.listen(PORT,async()=>{
     console.log(`App is running at ${PORT}`);
+    if(process.env.SYNC_DB)
+    {
+     db.sequelize.sync({alter:true});
+    }
+  
+    
     
 }) 
