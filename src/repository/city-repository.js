f77d384f192cb49da0ed5fabee125749f8805df0
Samrendra <samrendrakumar893@gmail.com>
2023-10-17 08:45:32 +0530

    fetched all cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -47,6 +47,17 @@ class CityRepository {
       }
 
     }
+
+    async getAllCities(){
+      try{
+        const cities = await City.findAll() ;
+        return cities ;
+      }catch(error)
+      {
+        console.log("Something went wrong in the repository layer ");
+        throw {error};
+      }
+    }
 }
 
 module.exports  = CityRepository
